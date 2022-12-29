e46e48bd0c1f300d309c6f8c00ce1e007a41c775
YogitaDevi <yogita.gry@gmail.com>
2022-12-29 22:20:11 +0530

    [yogita]Added: Data Management of Rice, Pulses and Wheats

---
+++ JSONInventory/JSONMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json;
+
+namespace JSONInventory
+{
+    public class JSONMain
+    {
+
+        public void ReadJsonFile(string filePath)
+        {                      
+           var json = File.ReadAllText(filePath);
+           List<model> data = JsonConvert.DeserializeObject<List<model>>(json);
+           Console.WriteLine("name: weight, price, total value");
+           foreach (var content in data)
+           {     
+              double value = content.price * content.weight;
+              Console.WriteLine(content.name + " : " + content.weight + " , " + content .price + " , " + value);
+
+           }
+            
+        }
+    }
+}
---
+++ JSONInventory/Program.cs
@@ -0,0 +1,4 @@
+﻿
+JSONInventory.JSONMain jSON = new JSONInventory.JSONMain();
+jSON.ReadJsonFile("E:\\RFP231\\JSON\\JSONInventory\\Inventory.json");
+
